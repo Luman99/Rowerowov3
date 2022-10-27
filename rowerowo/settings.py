b46fde08210d366068a9366e7a89b34f574561d6
Kamig95 <kamilgrab95@gmail.com>
2022-10-27 15:59:04 +0200

    debugfalse

--- rowerowo/settings.py
+++ rowerowo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0pq!36#4u@s0zaopn$^oydsw%nvzf&##(vm-cud$wne#8p0zc('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
